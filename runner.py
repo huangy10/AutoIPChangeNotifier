e926f9348e1f6fe115e3d31704dbfe72cbc4a05a
Woody Huang <woody1573@163.com>
2016-10-18 23:02:14 +0800

    move logfile into local directory

--- runner.py
+++ runner.py
@@ -13,50 +13,59 @@ PASSWORD = "<password>"
 SMTP_SERVER = "smtp.163.com"
 SMTP_SERVER_PORT = '25'
 
-logFile = '/var/log/autoipchangenotifier.log'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-request = urllib2.urlopen(
-    'http://www.baidu.com/s?wd=ip&rsv_spt=1&rsv_iqid=0xbefbb8610000747d&issp=1&f=8&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&rsv_sug3=4&rsv_sug1=3&rsv_sug7=100&rsv_sug2=0&inputT=1648&rsv_sug4=1648'
-)
+logFile = os.path.join(BASE_DIR, "autoipchangenotifier.log")
 
-soup = BeautifulSoup(request)
-ipResponse = soup.find('div', {'class': 'result-op c-container'}).get("fk")
-print ipResponse
-ip = re.search('[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+', str(ipResponse)).group(0)
 
-prevIp = ip
+def main():
+    request = urllib2.urlopen(
+        'http://www.baidu.com/s?wd=ip&rsv_spt=1&rsv_iqid=0xbefbb8610000747d&issp=1&f=8&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=baiduhome_pg&rsv_enter=1&rsv_sug3=4&rsv_sug1=3&rsv_sug7=100&rsv_sug2=0&inputT=1648&rsv_sug4=1648'
+    )
 
-try:
-    with open(logFile, 'r') as log:
-        prevIp = log.readline()
+    soup = BeautifulSoup(request)
+    ipResponse = soup.find('div', {'class': 'result-op c-container'}).get("fk")
+    print "Current IP: %s" % ipResponse
+    ip = re.search('[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+', str(ipResponse)).group(0)
+
+    prevIp = ip
+
+    try:
+        with open(logFile, 'r') as log:
+            prevIp = log.readline()
+            log.close()
+            print "Previous IP: %s" % prevIp
+    except IOError:
+        print "Fail to get previous ip"
+
+    if ip != prevIp:
+        target_emails = yaml.load(open(
+            os.path.join(os.path.dirname(__file__), 'emails.yaml')))['emails']
+        for recipient in target_emails:
+            msg = MIMEText("New IP address for {servername} is {ip}".format(
+                servername=SERVERNAME, ip=ip
+            ))
+            msg['Subject'] = 'IP address of {servername} Changed'.format(
+                servername=SERVERNAME
+            )
+            msg['From'] = EMAIL
+            print "Connecting to smtp server"
+            server = smtplib.SMTP(SMTP_SERVER, SMTP_SERVER_PORT)
+            server.ehlo()
+            server.starttls()
+            server.ehlo()
+            server.login(EMAIL, PASSWORD)
+
+            msg['To'] = recipient
+            print 'sending email'
+            server.sendmail(EMAIL, recipient, msg.as_string())
+            server.quit()
+            print "Success!"
+
+    with open(logFile, 'w') as log:
+        log.write(ip)
         log.close()
-except IOError:
-    pass
 
-if ip != prevIp:
-    target_emails = yaml.load(open(
-        os.path.join(os.path.dirname(__file__), 'emails.yaml')))['emails']
-    for recipient in target_emails:
-        msg = MIMEText("New IP address for {servername} is {ip}".format(
-            servername=SERVERNAME, ip=ip
-        ))
-        msg['Subject'] = 'IP address of {servername} Changed'.format(
-            servername=SERVERNAME
-        )
-        msg['From'] = EMAIL
-        print "Connecting to smtp server"
-        server = smtplib.SMTP(SMTP_SERVER, SMTP_SERVER_PORT)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-        server.login(EMAIL, PASSWORD)
 
-        msg['To'] = recipient
-        print 'sending email'
-        server.sendmail(EMAIL, recipient, msg.as_string())
-        server.quit()
-        print "Success!"
-
-with open(logFile, 'w') as log:
-    log.write(ip)
-    log.close()
+if __name__ == "__main__":
+    main()
